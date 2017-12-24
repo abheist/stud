7e0b57a8f095248c12f24f69970eab3dd1c1a777
Abhishek <abhishek291093@gmail.com>
2017-12-24 14:36:47 +0530

    AKS: change db to mysql

--- stud/settings.py
+++ stud/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'notes.apps.NotesConfig',
 ]
 
 MIDDLEWARE = [
@@ -76,9 +77,14 @@ WSGI_APPLICATION = 'stud.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'stud_db',
+        'USER': 'root',
+        'PASSWORD': 'kisanhub@123',
+        'HOST': '',
+        'POST': '33060',
     }
+
 }
 
 
